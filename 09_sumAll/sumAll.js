de07476c09f479c6ab0130a0e2f306922b1a1368
Definitively <45801523+Definitively@users.noreply.github.com>
2025-06-12 09:38:30 -0500

    Completed exercises 5 through 11.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,6 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    return Array.from({length: b + 1 - a}, (x, i) => i + a)
+    .reduce((acc, curr) => acc + curr, 0);
 };
 
 // Do not edit below this line
